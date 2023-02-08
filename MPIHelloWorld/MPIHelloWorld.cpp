e02d616fb4979ddee6fc6cd82675b712edf22e94
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-08 16:02:51 +0000

    Testing first C++ commit.

--- MPIHelloWorld/MPIHelloWorld.cpp
+++ MPIHelloWorld/MPIHelloWorld.cpp
@@ -6,12 +6,12 @@ int main(int argc, char** argv) {
 	MPI_Init(NULL, NULL);
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	std::cout << "Hellooooo " << world_rank << ", World: " << world_size << std::endl;
+	std::cout << "Hello commit " << world_rank << ", World: " << world_size << std::endl;
 	// finalise MPI and return control to the OS;
 	MPI_Finalize();
 	return 0;
 }
 
 
-//C:\Users\mauri\source\repos\MPIHelloWorld\x64\Debug\MPIHelloWorld.exe
+//C:\Users\mauri\source\repos\MPIHelloWorld\x64\Debug\
 //mpiexec -n 4 ./MPIHelloWorld.exe
