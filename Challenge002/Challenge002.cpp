6bb61a31eefbef62dac367ed2bed550c60daa358
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-20 17:05:35 +0000

    Challenge002 - setup

---
+++ Challenge002/Challenge002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <mpi.h>
+
+//Write an MPI program where:
+// each node generates a random number
+// &subsequently sends to the coordinator
+// which will then display the total sum
+
+int main(int argc, char** argv) {
+
+
+	return 0;
+}
