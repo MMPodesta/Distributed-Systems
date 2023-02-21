3d6bff78a6cfe4e62f59c2d1f21faa0cbd618fc2
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-21 20:22:18 +0000

    Challenge003-Setup

---
+++ Challenge003/Challenge003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <mpi.h>
+
+/*Write an MPI program where:
+each node will sleep for a random amount of time
+amount of time should be between 0-4
+print out “rank X sleeping” and the amount of time to sleep
+on awakening, the node will then synchronise with the others
+print “rank X synchronised” after synchronisation*/
+
+int main(int argc, char** argv) {
+
+	std::cout << "test" << std::endl;
+
+	return 0;
+}
