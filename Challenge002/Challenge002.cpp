a54a664960069e4081f54ff47042df1c772e44c8
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-21 20:12:42 +0000

    implementing challenge 002

--- Challenge002/Challenge002.cpp
+++ Challenge002/Challenge002.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mpi.h>
+#include <ctime>
 
 //Write an MPI program where:
 // each node generates a random number
@@ -7,7 +8,33 @@
 // which will then display the total sum
 
 int main(int argc, char** argv) {
+	//store current time ms
+	time_t now = time(nullptr);
+
+	MPI_Init(NULL, NULL);
+	
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	//seed random number
+	srand(world_rank + now);
+
+	// random and total variables
+	int our_sum = rand() % 100;
+	int total_sum = 0;
+
+	// 1 variable to be reduced, 2 destination variable contain reduced value 
+	// 3 count, 4 type, 5 reduce operation(ex:min,max), 6 root rank, communicator
+	MPI_Reduce(&our_sum, &total_sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	
+	// print out
+	std::cout << "rank " << world_rank << " our sum, total sum: " << our_sum <<
+		", " << total_sum << std::endl;
 
 
+	MPI_Finalize();
 	return 0;
 }
