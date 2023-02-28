a7d56ed83a0e2c6dfa695094441b513c4e048727
MauricioMurta <mauriciomurta13@hotmail.com>
2023-02-28 11:47:08 +0000

    Implementing challenge003 with synchronisation

--- Challenge003/Challenge003.cpp
+++ Challenge003/Challenge003.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <mpi.h>
+#include <Windows.h>
 
 /*Write an MPI program where:
 each node will sleep for a random amount of time
@@ -9,9 +10,27 @@ on awakening, the node will then synchronise with the others
 print “rank X synchronised” after synchronisation*/
 
 int main(int argc, char** argv) {
-	//DO APP 007 FIRST
+	MPI_Init(NULL, NULL);
 
-	std::cout << "test" << std::endl;
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	std::cout << "rank " << world_rank << " size " << world_size << std::endl;
+
+	// seed sleep
+	srand(world_rank);
+	int sleep_time = rand() % 5;
+	std::cout << "rank " << world_rank << " sleeping " << sleep_time << std::endl;
+
+	Sleep(sleep_time * 1000);
+	std::cout << "rank " << world_rank << " waking up and synchronizing" << std::endl;
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	std::cout << "rank " << world_rank << " synchronized" << std::endl;
+
+	MPI_Finalize();
 	return 0;
 }
